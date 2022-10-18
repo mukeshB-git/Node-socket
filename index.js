578d7933dd1adf8a20c104bbab5f152a3a876a17
mukesh <mbhagat@ee.iitr.ac.in>
2022-10-18 10:02:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`server is listening to port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/javascript', (req, res) => {
+    res.sendFile(__dirname + '/public/javascript.html')
+});
+
+app.get('/CSS', (req, res) => {
+    res.sendFile(__dirname + '/public/CSS.html')
+});
+
+app.get('/endpoint', (req, res) => {
+    console.log("you reached endpoint")
+    res.send("you reached endpoint")
+});
+
+
+const tech = io.of('/tech');
+
+tech.on('connection', (socket) => {
+
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `new user joined ${data.room} room.`);
+    })
+
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        tech.emit('message', 'user disconnected');
+    })
+})
